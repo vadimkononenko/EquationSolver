261698f2b742b64aeeae6533258d392c69350318
Xiaomi <vadiskon@gmail.com>
2022-02-13 13:12:16 +0200

    Feature: added interactive mode support

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        if(args.length == 0) {
+            double a = 0, b = 0, c = 0;
+
+            System.out.print("a = ");
+            a = readData();
+            System.out.print("b = ");
+            b = readData();
+            System.out.print("c = ");
+            c = readData();
+
+            printEquation(a,b,c);
+            equation(a,b,c);
+        } else {
+            
+        }
+    }
+
+    public static void printEquation(double a, double b, double c) {
+        System.out.println("Equation is: " + "(" + a + ") x^2 + (" + b + ") x + (" + c + ") = 0");
+    }
+
+    public static double readData(){
+        Scanner scanner = new Scanner(System.in);
+        if(scanner.hasNextDouble()) {
+            return scanner.nextDouble();
+        }
+        else{
+            String data = scanner.next();
+            System.out.println("Error. Expected a valid real number, got " + data + " instead");
+            return readData();
+        }
     }
 
     public static void equation(double a, double b, double c) {
